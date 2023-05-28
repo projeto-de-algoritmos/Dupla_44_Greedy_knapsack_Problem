3c18f3c581910b5a1fda0345eeaf407ea22b22d5
Carlos Daniel de Godoy Barros Nascimento <cdgodoy17@gmail.com>
2023-05-28 14:06:06 -0300

    Update main.py
    Ajustes gerais

--- main.py
+++ main.py
@@ -1,59 +1,6 @@
-from Tkinter import *
+#from Tkinter import *
 import tkinter as tk
 from tkinter.ttk import *
-
-class Passwordchecker(tk.Frame):
-   def __init__(self, parent):
-       tk.Frame.__init__(self, parent)
-       self.parent = parent
-       self.initialize_user_interface()
-
-   def initialize_user_interface(self):
-       self.parent.geometry("400x400")
-       self.parent.title("Pokeirmanos")
-       self.entry=tk.Entry(self.parent)
-       self.entry.pack()
-       self.button=tk.Button(self.parent,text="testa", command=self.PassCheck)
-       self.button.pack()
-       self.label=tk.Label(self.parent,text="Fala um pokemon ae")
-       self.label.pack()
-
-   def PassCheck(self):
-       password = self.entry.get()
-       if len(password)>=9 and len(password)<=12:
-          self.label.config(text="Acertou miseravi")
-       else:
-          self.label.config(text="ERRROOUUU")
- 
-# creates a Tk() object
-master = tk.Tk()
- 
-# sets the geometry of main
-# root window
-master.geometry("200x200")
- 
- 
-# function to open a new window
-# on a button click
-def openNewWindow():
-     
-    # Toplevel object which will
-    # be treated as a new window
-    #newWindow = tk.Toplevel(master)
-    root = tk.Tk()
-    run = Passwordchecker(root)
-    root.mainloop()
- 
-
-class KnapsackPackage(object):
-    """ Knapsack Package Data Class """
-    def __init__(self, weight, value):
-        self.weight = weight
-        self.value = value
-        self.cost = value / weight
-
-    def __lt__(self, other):
-        return self.cost < other.cost
 
 class FractionalKnapsack(object):
     def knapsackGreProc(self, W, V, M, n):
@@ -80,13 +27,97 @@ class FractionalKnapsack(object):
 
         print("Max Value: ", result)
 
+class Passwordchecker(tk.Frame):
+   def __init__(self, parent):
+       tk.Frame.__init__(self, parent)
+       self.parent = parent
+       self.initialize_user_interface()
+
+   def initialize_user_interface(self):
+       self.parent.geometry("600x600")
+       self.parent.title("Pokeirmanos")
+       self.label=tk.Label(self.parent,text="Insira a quantidade de itens")
+       self.label.configure(bg='#5E2C04', fg='#90EE90')
+       self.label.pack()
+       self.entry=tk.Entry(self.parent)
+       self.entry.pack()
+       self.label=tk.Label(self.parent,text="Digite o tamanho total da mochila")
+       self.label.configure(bg='#5E2C04', fg='#90EE90')
+       self.label.pack()
+       self.entry=tk.Entry(self.parent)
+       self.entry.pack()
+       self.label=tk.Label(self.parent,text="Insira os pesos dos itens")
+       self.label.configure(bg='#5E2C04', fg='#90EE90')
+       self.label.pack()
+       self.entry1=tk.Entry(self.parent)
+       self.entry1.pack()
+       self.label=tk.Label(self.parent,text="Insira os valores dos itens")
+       self.label.configure(bg='#5E2C04', fg='#90EE90')
+       self.label.pack()
+       self.entry2=tk.Entry(self.parent)
+       self.entry2.pack()
+       self.button=tk.Button(self.parent,text="Verificar", command=self.PassCheck)
+       self.button.pack()
+       
+   def PassCheck(self):
+        size = self.entry.get()
+        peso = self.entry.get()
+        IP = list(map(int, self.entry1.get().split()))
+        Valor = list(map(int, self.entry2.get().split()))
+        
+        # W = [30, 10, 2, 4] #-> peso do item
+        # V = [50, 25, 2, 6] #-> valor do item
+        # M = 37 #-> mochiila
+        # n = 4 #-> numero de itens
+        n = size
+        W = IP #-> IP = Item Peso
+        M = peso #-> peso total da mochila
+        V = Valor
+
+        proc = FractionalKnapsack()
+        proc.knapsackGreProc(W, V, M, n)
+       
+ 
+# creates a Tk() object
+master = tk.Tk()
+ 
+# sets the geometry of main
+# root window
+master.geometry("600x600")
+ 
+ 
+# function to open a new window
+# on a button click
+def openNewWindow():
+     
+    # Toplevel object which will
+    # be treated as a new window
+    #newWindow = tk.Toplevel(master)
+    root = tk.Tk()
+    root.configure(bg='#5E2C04')
+    run = Passwordchecker(root)
+    root.mainloop()
+ 
+
+class KnapsackPackage(object):
+    """ Knapsack Package Data Class """
+    def __init__(self, weight, value):
+        self.weight = weight
+        self.value = value
+        self.cost = value / weight
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+
 if __name__ == "__main__":
 
-    label = Label(master,
-                text ="Welcome stranger...")
-    
+    label = Label(master, text="Welcome Stranger...")
     label.pack(pady = 10)
-    
+    #label.configure(bg='#5E2C04')
+
+    #texto = tk.Label(master, text="Salve esquisito...", bg='#5E2C04', activebackground="#BC03FC")
+
     master.title('Mercador Medieval')
     # a button widget which will open a
     # new window on button click
@@ -99,11 +130,7 @@ if __name__ == "__main__":
                 text ="Sair",
                 command = quit)
     btn2.pack(pady = 20) 
+    master.configure(bg='#5E2C04')
     # mainloop, runs infinitely
     tk.mainloop()
-    W = [15, 10, 2, 4]
-    V = [30, 25, 2, 6]
-    M = 37
-    n = 4
-    proc = FractionalKnapsack()
-    proc.knapsackGreProc(W, V, M, n)
+    
